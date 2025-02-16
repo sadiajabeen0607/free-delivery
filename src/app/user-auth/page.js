8b7852a3023ad15c0be0e87b29291ed9e4eb33a4
Sadia <sadiajabeen0607@gmail.com>
2025-02-16 22:53:02 +0500

    Fixed Suspense issue in UserAuth component

--- src/app/user-auth/page.js
+++ src/app/user-auth/page.js
@@ -1,17 +1,21 @@
+"use client";
+import { Suspense } from "react";
 import CustomerHeader from "../_components/CustomerHeader";
 import Footer from "../_components/Footer";
-import UserSignUpLoginPage from "../_components/UserSignUpLoginPage"
+import UserSignUpLoginPage from "../_components/UserSignUpLoginPage";
 
 const UserAuth = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <CustomerHeader />
       <div className="flex-grow">
-        <UserSignUpLoginPage />
+        <Suspense fallback={<div>Loading...</div>}>
+          <UserSignUpLoginPage />
+        </Suspense>
       </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default UserAuth;
